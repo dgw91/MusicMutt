e573b58b5884c8d9d04f28c303f86b2126ce1b3e
Tanner Burge-Beckley <tburgeb@siue.edu>
2017-03-26 07:36:43 -0500

    FInal styling and script for alpha demo

---
+++ static/js/home.js
@@ -0,0 +1,30 @@
+var number = document.getElementById('number');
+var form = document.getElementsByTagName('form')[0];
+number.addEventListener('blur', function(){ checkNumber(); }, false);
+form.addEventListener('submit', function(event){ checkForm(event); }, false);
+
+function checkNumber() {
+    console.log("Checking the number");
+    span = document.getElementsByClassName('error')[0];
+    if (number.value < 1 || !number.value) {
+        span.textContent = "Please pick how many tracks you would like in your playlist";
+    }
+    else if (number.value > 25) {
+        span.textContent = "Number of tracks cannot exceed 25";
+        number.value = null;
+    }
+    else {
+        span.textContent = "";
+    }
+}
+
+function checkForm(e) {
+    console.log("Checking the form");
+    checkNumber();
+
+    span = document.getElementsByClassName('error')[0];
+
+    if (span.textContent != "") {
+        e.preventDefault();
+    }
+}
